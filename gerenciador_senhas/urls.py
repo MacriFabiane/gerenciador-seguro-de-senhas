944790bf57f9ffe3e54e9cf8922701b490787098
brunonunesdasilva <brun0.silva6358@gmail.com>
2025-06-30 22:51:57 -0300

    feat: csss

--- gerenciador_senhas/urls.py
+++ gerenciador_senhas/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.pag_principalView, name='pag_principalView'),
+    path('gerenciador_senhas/pag_principal', views.pag_principalView, name='pag_principalView'),
     path('gerenciador_senhas/pag_edicao', views.pag_edicaoView, name='pag_edicaoView')
 ]
-
--- gerenciador_senhas/views.py
+++ gerenciador_senhas/views.py
@@ -3,11 +3,20 @@ from django.contrib.auth.models import User
 
 # Create your views here.
 def pag_principalView(request):
-    user = request.user  # pega o usuário logado
-    senhas_salvas = []
+    # user = request.user  # pega o usuário logado
+    # senhas_salvas = []
     
 
-    return render(request, 'gerenciador_senhas/pag_principal.html', {'user':user, 'senhas_salvas': senhas_salvas})
+    # return render(request, 'gerenciador_senhas/pag_principal.html', {'user':user, 'senhas_salvas': senhas_salvas})
+    return render(request, 'gerenciador_senhas/pag_principal.html', {
+    'user': {'name':'Maria'},
+    'senhas_salvas': [
+        {'apps_url': 'https://facebook.com/', 'usuario': 'maria_fb', 'senha': 'senha_fb123'},
+        {'apps_url': 'https://gmail.com/', 'usuario': 'maria.gm', 'senha': 'senha_gmail!'},
+        {'apps_url': 'https://linkedin.com/', 'usuario': 'maria_li', 'senha': 'senhaLinkedin2024'},
+        {'apps_url': 'https://github.com/', 'usuario': 'maria_git', 'senha': '1234segura'},
+    ]
+    })
 
 def pag_edicaoView(request):
     return(request, 'gerenciador_senhas/pag_edicao.html')
